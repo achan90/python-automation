19edb4fb12c4ed162c28d884dbdd8ae1d5489148
achan90 <pousi.pekka@gmail.com>
2017-01-04 21:34:20 +0200

    Update ipn.py

--- ipn.py
+++ ipn.py
@@ -4,12 +4,25 @@ import re
 
 
 def isPhoneNumber(message):
-    regex = re.compile(r"(\d{3}-)?(\d{3}-\d{4})")
+    regex = re.compile(
+        r"""
+        ( \d{3} [-\s)] | \( \d{3} \) ) ? # Area code
+
+        ( [-\s] ) ?                      # Separator
+
+        ( \d{3} )                        # First three numbers
+
+        ( [-\s] ) ?                      # Separator
+
+        ( \d{4} )                        # Last four numbers
+
+        """, re.VERBOSE)
+
     match = regex.findall(message)
 
     for x in match:
         print("Found phone number: {}".format("".join(x)))
 
 
-message = "Call me at 415-555-1011 tomorrow. 415-555-9999 is my office."
+message = "Call me at 415-555-1011 tomorrow. (415) 555 9999 is my office."
 isPhoneNumber(message)

d505b746393b19eaeda34a6de67e95f32b5cba60
achan90 <pousi.pekka@gmail.com>
2017-01-14 13:17:53 +0200

    Update pw.py

--- pw.py
+++ pw.py
@@ -1,11 +1,10 @@
 #! /usr/bin/env python3
 
 import pyperclip
+import shelve
 
 # Still need to figure out how to encrypt the passwords.
-PASSWORDS = {"email": "dkapdkpakdpakdpakdpakgpoampd",
-             "reddit": "pdkapdkapkfgpak",
-             "facebook": "fpakfpakfkaåfkåa"}
+PASSWORDS = shelve.open("passwords")
 
 
 account = input("Enter account name: ")
@@ -19,14 +18,10 @@ else:
         print("There is no account named {}".format(account))
         add_account = input("Do you wish to add it? Y/N: ")
 
-        # Still need to figure how to permanently save the updated dictionary.
-        # Dedicated password file that can be imported could possibly work.
         if add_account.lower() == "y":
-            PASSWORDS.setdefault(account, input(
-                "Enter {} password: ".format(account)))
+            PASSWORDS[account] = input(
+                "Enter {} password: ".format(account))
             print("Password for {} saved".format(account))
-            # Used for testing, will be removed once script is completed.
-            print(PASSWORDS)
             exit()
 
         elif add_account.lower() == "n":
@@ -34,3 +29,5 @@ else:
 
         else:
             continue
+
+PASSWORDS.close()

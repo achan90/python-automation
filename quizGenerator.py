db1b0a5ae7bb6d2bef5959102a6df520d7f226ac
achan90 <pousi.pekka@gmail.com>
2017-01-14 11:21:06 +0200

    Update quizGenerator.py

--- quizGenerator.py
+++ quizGenerator.py
@@ -1,9 +1,7 @@
 #! /usr/bin/env python3
 
-import shelve
-import os
-import pyperclip
 import random
+import os
 
 # The quiz data. Keys are states and values are their capitals.
 capitals = {"Alabama": "Montgomery",
@@ -57,15 +55,63 @@ capitals = {"Alabama": "Montgomery",
             "Wisconsin": "Madison",
             "Wyoming": "Cheyenne"}
 
-# Generate 35 quiz files
-for num in range(35):
-    # Create the quiz and answer key files
-    with open("quiz{}.txt".format(num + 1), "w+") as quiz:
-        pass
+# shuffle the order of the states.
+states = list(capitals.keys())
+random.shuffle(states)
 
-    with open("quiz{}Answer.txt".format(num + 1), "w+") as answer:
-        pass
+# Set quiz directory.
+os.chdir(
+    os.path.join(
+        "/home", "peksi", "Documents", "python3", "projects",
+        "repositories", "python-automation", "quizzes"))
+quizDirectory = os.getcwd()
+print("Quiz and answer files generated to {}".format(quizDirectory))
 
-        # Write out header for the quiz
-        # Shuffle the order of the states
-        # Loop through all 50 states, making questions for each
+
+# Generate 35 quiz and answer key files
+def quizGenerator():
+    # Create quiz and aswer key files.
+    for num in range(35):
+        # Create quiz files
+        with open("quiz{}.txt".format(num + 1), "w") as quiz:
+            quiz.write("Name:\n\nDate:\n\nPeriod:\n\n")
+            quiz.write(
+                (" " * 20) + "State Capital Quiz (Form {})\n\n".format(
+                    num + 1))
+
+            # write the question and answer options to the quiz file.
+            for questions in range(50):
+                correctAnswer = capitals[states[questions]]
+                wrongAnswers = list(capitals.values())
+                del wrongAnswers[wrongAnswers.index(correctAnswer)]
+                wrongAnswers = random.sample(wrongAnswers, 3)
+                answerOptions = wrongAnswers + [correctAnswer]
+                random.shuffle(answerOptions)
+
+                quiz.write(
+                    "{}. What is the capital of {}?\n".format(
+                        questions + 1, states[questions]))
+                for i in range(4):
+                    quiz.write(
+                        " {}. {}\n".format("ABCD"[i], answerOptions[i]))
+                quiz.write("\n")
+
+        # create answer key files.
+        with open("quizAnswer{}.txt".format(num + 1), "w") as answers:
+
+            # write the answer key to a file.
+            for questions in range(50):
+                correctAnswer = capitals[states[questions]]
+                wrongAnswers = list(capitals.values())
+                del wrongAnswers[wrongAnswers.index(correctAnswer)]
+                wrongAnswers = random.sample(wrongAnswers, 3)
+                answerOptions = wrongAnswers + [correctAnswer]
+                random.shuffle(answerOptions)
+
+                answers.write(
+                    "{}. {}\n".format(
+                        questions + 1, "ABCD"[answerOptions.index(
+                            correctAnswer)]))
+
+
+quizGenerator()

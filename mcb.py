3c78ecf517e2a6992792def3df1430c103abe102
achan90 <pousi.pekka@gmail.com>
2017-01-14 15:05:38 +0200

    Update mcb.py

--- mcb.py
+++ mcb.py
@@ -5,6 +5,10 @@
 #   ./mcb.py save <keyword> - Saves clipboard to keyword.
 #   ./mcb.py <keyword> - Loads data from keyword to clipboard.
 #   ./mcb.py list - Loads all keywords to clipboard.
+#   ./mcb.py delete <keyword> - Deletes keyword data.
+#   -/mcb.py reset - Deletes data from all keywords.
+
+# This script is not tested, might not work as intended.
 
 import shelve
 import pyperclip
@@ -15,6 +19,14 @@ mcbShelf = shelve.open("mcb")
 # Save clipboard content.
 if len(sys.argv) == 3 and sys.argv[1].lower() == "save":
     mcbShelf[sys.argv[2]] = pyperclip.paste()
+
+# Delete keyword data
+elif len(sys.argv) == 3 and sys.argv[1].lower == "delete":
+    del mcbShelf[sys.argv[2]]
+
+# Delete all keyword data
+elif len(sys.argv) == 2 and sys.argv[1] == "reset":
+    del mcbShelf
 
 # List keywords and load content.
 elif len(sys.argv) == 2:

54f6bec1ebe8d108d1de6930a19cc5020cba0216
achan90 <pousi.pekka@gmail.com>
2017-01-15 08:48:11 +0200

    Update madLibs.py

--- madLibs.py
+++ madLibs.py
@@ -1,2 +1,45 @@
 #! /usr/bin/env python3
 
+import re
+import os
+
+
+def pathLocator(file):
+    filePath = os.path.join(file)
+    return filePath
+
+
+def madLibs():
+    regex = re.compile(r"(ADJECTIVE)|(VERB)|(NOUN)")
+    file = open(pathLocator(input("Enter path name:  ")), "r")
+    fileInput = file.read()
+    match = regex.search(fileInput)
+
+    while match is not None:
+        if match.group(1) is not None:
+            fileInput = regex.sub(
+                input("Enter adjective:  "), fileInput, count=1)
+            match = regex.search(fileInput)
+
+        elif match.group(2) is not None:
+            fileInput = regex.sub(
+                input("Enter verb:  "), fileInput, count=1)
+            match = regex.search(fileInput)
+
+        elif match.group(3) is not None:
+            fileInput = regex.sub(
+                input("Enter noun:  "), fileInput, count=1)
+            match = regex.search(fileInput)
+
+        else:
+            break
+
+    print(fileInput)
+    os.chdir(pathLocator(input("Enter directory name:  ")))
+    newFile = open("madLibbed.txt", "w")
+    newFile.write(fileInput)
+    file.close()
+    newFile.close()
+
+
+madLibs()
